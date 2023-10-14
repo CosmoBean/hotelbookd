67b9a57deaa7768cc217e5b7b2a18c42f0e5e501
CosmoBean <freelance.ein@gmail.com>
2023-10-15 01:38:36 +0530

    feat: config env file

--- main.go
+++ main.go
@@ -2,9 +2,17 @@ package main
 
 import (
 	"github.com/CosmoBean/hotelbookd/server"
+	"github.com/joho/godotenv"
+	"log"
 )
 
 func main() {
+
+	err := godotenv.Load()
+	if err != nil {
+		log.Panic("error Loading the env file: ", err)
+	}
+
 	server.Init()
 
 }
