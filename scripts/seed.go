ca0e1da646fc2aa34f18f2ebd934218330904183
cosmobean <cosmobean.dev@gmail.com>
2023-12-05 11:10:01 +0530

    refactor: seed.go

--- scripts/seed.go
+++ scripts/seed.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"context"
-	"fmt"
 	"log"
 
 	"github.com/CosmoBean/hotelbookd/db"
@@ -10,20 +9,20 @@ import (
 	"github.com/CosmoBean/hotelbookd/utils"
 	"github.com/joho/godotenv"
 	"go.mongodb.org/mongo-driver/bson/primitive"
+	"go.mongodb.org/mongo-driver/mongo"
 )
 
-func main() {
-	err := godotenv.Load()
-	if err != nil {
-		log.Panic("error Loading the env file: ", err)
-	}
-	ctx := context.Background()
-	client := db.GetMongoClient()
-	dbName := utils.GetEnvDefault("MONGO_DBNAME", "hotel-reservation")
-	hotelStore := db.NewMongoHotelStore(client, dbName)
+var (
+	client     *mongo.Client
+	roomStore  db.RoomStore
+	hotelStore db.HotelStore
+	ctx        = context.Background()
+)
+
+func seedHotel(hotelName, hotelLocation string) {
 	hotel := models.Hotel{
-		Name:     "Bellucia",
-		Location: "France",
+		Name:     hotelName,
+		Location: hotelLocation,
 		Rooms:    []primitive.ObjectID{},
 	}
 	rooms := []models.Room{
@@ -46,18 +45,32 @@ func main() {
 		log.Fatal("cannot insert hotel -->", err)
 	}
 
-	roomStore := db.NewMongoRoomStore(client, dbName)
-
 	for _, room := range rooms {
 		room.HotelId = insertedHotel.Id
 
-		insertedRoom, err := roomStore.InsertRoom(ctx, &room)
+		_, err := roomStore.InsertRoom(ctx, &room)
 		if err != nil {
 			log.Fatal("cannot insert room-->", err)
 		}
-		fmt.Println(insertedRoom)
 	}
+}
 
-	fmt.Println(insertedHotel)
+func init() {
+	err := godotenv.Load()
+	if err != nil {
+		log.Panic("error Loading the env file: ", err)
+	}
+	client = db.GetMongoClient()
+	dbName := utils.GetEnvDefault("MONGO_DBNAME", "hotel-reservation")
+	if err := client.Database(dbName).Drop(ctx); err != nil {
+		log.Fatal("unable to drop the database")
+	}
+	hotelStore = db.NewMongoHotelStore(client, dbName)
+	roomStore = db.NewMongoRoomStore(client, dbName)
+}
 
+func main() {
+	seedHotel("Bellucia", "France")
+	seedHotel("the Cozy Hotel", "Netherlands")
+	seedHotel("OYO", "India")
 }
